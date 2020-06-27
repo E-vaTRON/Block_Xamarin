0007cd7f5f7628d7d5f06e61c373c71c84a9f0de
E-va TRON <envaroy@gmail.com>
2020-06-27 18:22:24 +0700

    Block

---
+++ IceLand/IceLand/ViewModel/MainViewModel.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace IceLand.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+    }
+}
